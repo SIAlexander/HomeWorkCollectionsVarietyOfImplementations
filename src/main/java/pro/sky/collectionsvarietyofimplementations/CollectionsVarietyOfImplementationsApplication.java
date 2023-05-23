b9b75613903687e25884342dc0fe3d5a7b394320
Smokvin <alex.smokvin@gmail.com>
2023-05-23 20:07:22 +0300

    initial commit

---
+++ src/main/java/pro/sky/collectionsvarietyofimplementations/CollectionsVarietyOfImplementationsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.collectionsvarietyofimplementations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsVarietyOfImplementationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionsVarietyOfImplementationsApplication.class, args);
+    }
+
+}
